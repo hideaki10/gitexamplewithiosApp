30c2917d3dc77ecf9f3471d07a9bd54eccd2830e
qian <senshikou1985@gmail.com>
2021-03-05 16:16:33 +0900

    Initial Commit

---
+++ Shared/gitexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitexampleApp.swift
+//  Shared
+//
+//  Created by 銭之光 on 2021/03/05.
+//
+
+import SwiftUI
+
+@main
+struct gitexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
